663fdfb9dcff144cb6d5902b4ee004416861f9b0
Erkan Vatan <evatan@t3gemstone.org>
2025-03-15 12:04:34 +0300

    Fix distro build errors caused by t3gemstone apt repo missing release files
    - Currently only Ubuntu Jammy distro is served at t3gemstone apt repo.

--- distro/scripts/all/setup-gemstone-apt.sh
+++ distro/scripts/all/setup-gemstone-apt.sh
@@ -12,6 +12,7 @@ mkdir -p /etc/apt/keyrings
 
 curl -fsSL https://packages.t3gemstone.org/apt/gemstone-packages-keyring.gpg -o /etc/apt/keyrings/gemstone-packages-keyring.gpg
 
-echo "deb [signed-by=/etc/apt/keyrings/gemstone-packages-keyring.gpg] https://packages.t3gemstone.org/apt $DISTRO_SUITE main" | tee /etc/apt/sources.list.d/gemstone.list
-
-apt-get update -y
+if [[ "$DISTRO_SUITE" == "jammy" ]]; then
+    echo "deb [signed-by=/etc/apt/keyrings/gemstone-packages-keyring.gpg] https://packages.t3gemstone.org/apt $DISTRO_SUITE main" | tee /etc/apt/sources.list.d/gemstone.list
+    apt-get update -y
+fi
