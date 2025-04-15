a8c8bb42ed573086273592c973c0f4f17075c560
Muhammet Onur Bayraktar <mobayraktar@t3gemstone.org>
2025-04-15 12:43:45 +0300

    Updated package server address

--- distro/scripts/all/setup-gemstone-apt.sh
+++ distro/scripts/all/setup-gemstone-apt.sh
@@ -13,6 +13,6 @@ mkdir -p /etc/apt/keyrings
 curl -fsSL https://packages.t3gemstone.org/apt/gemstone-packages-keyring.gpg -o /etc/apt/keyrings/gemstone-packages-keyring.gpg
 
 if [[ "$DISTRO_SUITE" == "jammy" ]]; then
-    echo "deb [signed-by=/etc/apt/keyrings/gemstone-packages-keyring.gpg] https://packages.t3gemstone.org/apt $DISTRO_SUITE main" | tee /etc/apt/sources.list.d/gemstone.list
+    echo "deb [signed-by=/etc/apt/keyrings/gemstone-packages-keyring.gpg] https://packages.t3gemstone.org/apt/$MACHINE $DISTRO_SUITE main" | tee /etc/apt/sources.list.d/gemstone.list
     apt-get update -y
 fi
