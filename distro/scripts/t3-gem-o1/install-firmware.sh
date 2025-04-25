2b9cfd59b8627206706570c8008bb9a10a2595a7
akaybayram0 <bakay@t3gemstone.org>
2025-04-25 11:10:17 +0300

    added neccessary drivers and firmwares for t3-gem-o1 board

--- distro/scripts/t3-gem-o1/install-firmware.sh
+++ distro/scripts/t3-gem-o1/install-firmware.sh
@@ -6,7 +6,7 @@ WORKDIR="$1"
 ROOTDIR="$2"
 
 mkdir -p "$ROOTDIR/lib/firmware/ti-connectivity"
-cp -f "$WORKDIR/src/cc33xx-fw/"*.bin "$ROOTDIR/lib/firmware/ti-connectivity"
+cp -f "$WORKDIR/src/wl18xx-fw/"*.bin "$ROOTDIR/lib/firmware/ti-connectivity"
 
 cp -ar "$WORKDIR/src/ti-img-rogue-umlibs/targetfs/j721s2_linux/lws-generic/release/etc/"* "$ROOTDIR/etc"
 cp -ar "$WORKDIR/src/ti-img-rogue-umlibs/targetfs/j721s2_linux/lws-generic/release/usr/"* "$ROOTDIR/usr"
