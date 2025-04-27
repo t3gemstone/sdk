31caf1f1e986e479d9d8ca233d598242cea0bdf9
Gemstone <dev@t3gemstone.org>
2025-04-27 10:56:49 +0300

    Fix wrong image type

--- distro/scripts/qemuarm64/clean-up.sh
+++ distro/scripts/qemuarm64/clean-up.sh
@@ -4,6 +4,6 @@ export DEBIAN_FRONTEND=noninteractive
 
 DISTRO_TYPE="${1}"
 
-if [[ "$DISTRO_TYPE" == "desktop" || "$DISTRO_TYPE" == "gui" ]]; then
+if [[ "$DISTRO_TYPE" == "desktop" || "$DISTRO_TYPE" == "kiosk" ]]; then
     echo 'apt-get remove'
 fi
