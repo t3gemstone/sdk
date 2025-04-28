0bff13007d05b0f7f0a834c4d25c5b4337727df5
halak0013 <zumruduanka0013@gmail.com>
2025-04-28 11:32:24 +0300

    Add setup-gpiod script and conditional action for beagley-ai and t3-gem-o1

---
+++ distro/scripts/all/setup-gpiod.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+
+export DEBIAN_FRONTEND=noninteractive
+
+ROOTDIR="$1"
+
+
+if ! getent group gpio > /dev/null; then
+    groupadd gpio
+else
+    echo "[i] GPIO grup already exist."
+fi
+
+
+
+if groups "" | grep -q '\bgpio\b'; then
+    echo "[i] The user is already in the GPIO group ."
+else
+    usermod -a -G gpio gemstone
+fi
+
+UDEV_RULE="$ROOTDIR/etc/udev/rules.d/99-gpio.rules"
+echo "[+] Generating Udev udev rule..."
+echo 'SUBSYSTEM=="gpio", KERNEL=="gpiochip*", GROUP="gpio", MODE="660"' | tee $UDEV_RULE
